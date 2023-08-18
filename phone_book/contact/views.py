f2542619c25ffad066f338035ab01032ae991026
Bakr Frag <bakr.frag@gmail.com>
2023-08-18 16:53:14 +0300

    add viewset for contacts

--- phone_book/contact/views.py
+++ phone_book/contact/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework import mixins 
+from rest_framework.response import Response
+from contact.models import Contact , PhoneNumber 
+           
+from contact.serializers import ContactSerializer
+
+class ContactViewsets(mixins.CreateModelMixin , mixins.ListModelMixin, mixins.RetrieveModelMixin , viewsets.GenericViewSet):
+    """
+    handle operation on model contact 
+    create contact and associate with mulipli numbers 
+    retrieve single object
+    list of contacts with username and attached numbers
+    """
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
