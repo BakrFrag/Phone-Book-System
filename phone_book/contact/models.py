bd39ab32e5e069b373c643997981c415eaedfe3a
Bakr Frag <bakr.frag@gmail.com>
2023-08-18 16:32:19 +0300

    add contact model

--- phone_book/contact/models.py
+++ phone_book/contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    """
+    reprsents contact name , contact name unique
+    """
+    username = models.CharField(max_length=255 , null=False , blank=False)
+    def __str__(self):
+        return self.username
